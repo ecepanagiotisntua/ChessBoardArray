b108d63eda32f1ac96173811d7d330728f7ad3eb
ecepanagiotisntua <pkaoukis.2001@yahoo.gr>
2020-04-25 14:24:42 +0000

    loc Row class Const row class operator <<

--- chessboard.cpp
+++ chessboard.cpp
@@ -5,9 +5,13 @@ class ChessBoardArray {
 protected:
 
 T *data;
-int baseRow, baseCol;
-unsigned numRows, numCols;
-unsigned loc(inti, intj) constthrow(out_of_range);
+int base;
+unsigned num;
+unsigned loc(int i, int j) const throw(out_of_range){
+  int di=i-base, dj=j-base;  
+      if (di<0 || dj<0 || di>=n || dj>=n || (di+dj)%2!=0) throw out_of_range("wrong index");
+      
+}
 
 class Row {
   public:
@@ -22,16 +26,43 @@ public:
 };
 
 public:
- ChessBoardArray(unsigned size = 0, unsigned base = 0);
- ChessBoardArray(const ChessBoardArray &a);
+ ChessBoardArray(unsigned size = 0, unsigned base = 0)
+ : n(size), base(0), data(new int[(n*n+1)/2]){
+   for (int i = 0 ; i < (n*n+1)/2 ; i++ ) data[i] = 0;
+ }
+ ChessBoardArray(const ChessBoardArray &a){
+   n(a.n);
+   base(a.base);
+   data(new int [(n*n+1)/2]);
+   for (int i = 0; i < (n*n+1)/2; i++) data[i] = a.data[i];
+ }
+
  ~ChessBoardArray();
+ {
+   delete [] data;
+ }
 
- ChessBoardArray & operator = (const ChessBoardArray &a); {
+ ChessBoardArray & operator = (const ChessBoardArray &a){
+   delete [] data;
+   n(a.n);
+   base(a.base);
+   data(new int [(n*n+1)/2]);
+   for (int i = 0; i < (n*n+1)/2; i++) data[i] = a.data[i];
+   return *this;
+ }
 
- int & select(int i, int j);
- int select(int i, int j) const;
- const Row operator [] (int i);
- const ConstRow operator [] (int i) const;
+ int & select(int i, int j){
+   return data[loc(i,j)];
+ }
+ const int select(int i, int j) const{
+   return data[loc(i,j)];
+ }
+ const Row operator [] (int i) {
+   return Row(*this, i);
+ }
+ const ConstRow operator [] (int i) const{
+   return ConstRow(*this, i);
+ }
 
  friend ostream & operator << (ostream &out, const ChessBoardArray &a);
 };
